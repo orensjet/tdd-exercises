1b1cbd7197a71edc774ca134ff181f1a97af20f9
Aisawan Bosarakham <aisawan.bosa@ibm.com>
2019-11-20 13:15:36 -0600

    finished palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,3 +1,32 @@
+let isPalindrome = (phrase) => {
+    if(!phrase || phrase.trim().length === 0) return false;
+    return phrase.replace(/\W+/g,'').toLowerCase() === phrase.split('').reverse().join('').replace(/\W+/g, '').toLowerCase();
+}
+
 describe('a palindrome function', () => {
-    it('canary shows the infrastructure works');
+    it('canary shows the infrastructure works',() => {
+        expect(true).toBe(true);
+    });
+
+    it('radar is a palindrome', () => {
+        expect(isPalindrome('radar')).toBe(true);
+    });
+    it('dog is not a palindrome', () => {
+        expect(isPalindrome('dog')).toBe(false);
+    });
+    it('Race, car is a palindrome', () => {
+        expect(isPalindrome('Race, car')).toBe(true);
+    });
+    it('mom dad mom is a palindrome', () => {
+        expect(isPalindrome('mom dad mom')).toBe(true);
+    });
+    it('"" is not a palindrome', () => {
+        expect(isPalindrome('')).toBe(false);
+    });
+    it('"   " is not a palindrome', () => {
+        expect(isPalindrome('   ')).toBe(false);
+    });
+    it('undefined is not a palindrome', () => {
+        expect(isPalindrome()).toBe(false);
+    });
 });
