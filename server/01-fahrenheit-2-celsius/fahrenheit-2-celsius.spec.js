939c848c72bf12df9df1d3d9d7314cf3c051a94d
Aisawan Bosarakham <aisawan.bosa@ibm.com>
2019-11-20 11:32:49 -0600

    finished fahrenheit

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,5 +1,22 @@
+let fahrenheit2Celsius = fahrenheit => (fahrenheit - 32) * 5/9;
+
 describe('the fahrenheit-2-celcius canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
+
+   it ('fahrenheit2Celsius of 32 is 0',() => {
+     expect(fahrenheit2Celsius(32)).toBe(0);
+   });
+
+   it ('fahrenheit2Celsius of 212 is 100', () => {
+    expect(fahrenheit2Celsius(212)).toBe(100);
+   });
+
+   it ('fahrenheit2Celsius of 50 is 10', () => {
+    expect(fahrenheit2Celsius(50)).toBe(10);
+   });
+   it ('fahrenheit2Celsius of -40 is -40', () => {
+    expect(fahrenheit2Celsius(-40)).toBe(-40);
+   });
 });
