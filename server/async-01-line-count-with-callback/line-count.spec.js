5d54c2513a87a145c044506811209bd045ebed20
Aisawan Bosarakham <aisawan.bosa@ibm.com>
2019-11-21 14:27:09 -0600

    finished lineCount

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,9 +1,24 @@
+const {lineCount} = require('./line-count')
 // eslint-disable-next-line no-unused-vars
 const filename = 'server/async-01-line-count-with-callback/line-count.js';
 
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 12');
+
+  it('for file —— line-count.js —— should be 12', (done) => {
+    let onSuccess = (numberOfLines) => {
+      expect(numberOfLines).toBe(12);
+      done();
+    }
+    lineCount(filename, null, onSuccess)
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+      'is-not-there.js', (done) => {
+        let onError = (msg) => {
+          expect(msg).toBe('problem reading file: is-not-there.js');
+          done();
+        }
+        lineCount('is-not-there.js', onError, null);
+      });
+
 });
